d3741bd79efb80a6c5117959a5797aaee6a41b77
sreerangk <sreerangk77@gmail.com>
2022-07-15 23:36:11 +0530

    +

---
+++ +pattern.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    printf("enter the limit ");
+    scanf("%d",&n);
+  for(i=1;i<=n;i++)  
+  {  
+    if(i==((n/2)+1))  
+    {  
+      for(j=1;j<=n;j++)  
+      {  
+        printf("+");  
+      }  
+   
+    }  
+    else  
+    {  
+    for(j=1;j<=n/2;j++)  
+    {  
+      printf(" ");  
+    }  
+    printf("+");  
+    }  
+    printf("\n");  
+  }  
+  return 0;  
+}  
