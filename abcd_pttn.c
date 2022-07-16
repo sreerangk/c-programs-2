bb37e808e2c1ef2cda57e13155d6e3ffc63a3164
sreerangk <sreerangk77@gmail.com>
2022-07-16 14:17:45 +0530

    str ABC

--- abcd_pttn.c
+++ abcd_pttn.c
@@ -1,12 +1,24 @@
+// ABCDEFGFEDCBA
+// ABCDEF FEDCBA
+// ABCDE   EDCBA
+// ABCD     DCBA
+// ABC       CBA
+// AB         BA
+// A           A
 #include<stdio.h>
-int main(){
-    int n,i,j,k;
-    printf("enter the limit ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-       for(j=1;j<=i;j++){
-           printf("%d",64);
-       }
-       printf("\n");
+int main()
+{
+    int i,j,k,l,m;
+    for(i=0;i<=6;i++)
+    {
+        for(k=65;k<=71-i;k++)
+            printf("%c",k);
+        for(j=1;j<=i*2-1;j++)
+            printf(" ");
+        for(l=71-i;l>=65;l--)
+            if(l!=71)
+                printf("%c",l);
+        printf("\n");
     }
+    return 0;
 }
